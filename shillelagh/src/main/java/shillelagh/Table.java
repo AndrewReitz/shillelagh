78aafcc35f617d4e7d8ddc291393ddc7ea897a59
Andrew Reitz <aj.reitz@gmail.com>
2014-03-21 23:25:11 -0500

    Creating Basic Annotations
    Creating the most basic anotations for creating table models.

---
+++ shillelagh/src/main/java/shillelagh/Table.java
@@ -0,0 +1,12 @@
+package shillelagh;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+@Retention(CLASS) @Target(TYPE)
+public @interface Table {
+    String tableName() default "";
+}
