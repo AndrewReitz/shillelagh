5d3582aca426c194a15e849fd8b2afaf834c3f17
Andrew Reitz <aj.reitz@gmail.com>
2014-03-25 22:51:42 -0500

    Implemented simple database creation

--- shillelagh/src/main/java/shillelagh/Shillelagh.java
+++ shillelagh/src/main/java/shillelagh/Shillelagh.java
@@ -1,14 +1,60 @@
 package shillelagh;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
-public class Shillelagh {
+import java.lang.reflect.Method;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import shillelagh.internal.ShillelaghInjector;
+import shillelagh.internal.ShillelaghProcessor;
+
+public final class Shillelagh {
     private Shillelagh() {
         // No instantiation
     }
 
-    public static void createTable(SQLiteDatabase database, Object tableObject) {
+    static final Map<Class<?>, Class<?>> CACHED_CLASSES = new LinkedHashMap<>();
 
+    private static final String TAG = "Shillelagh";
+    private static boolean debug = false;
+
+    /** Turn on/off debug logging. */
+    public static void setDebug(boolean debug) {
+        Shillelagh.debug = debug;
+    }
+
+    public static void createTable(SQLiteDatabase database, Class<?> tableObject) {
+        try {
+            final Class<?> shillelagh = findShillelaghForClass(tableObject);
+            final Method method = shillelagh.getMethod(ShillelaghInjector.CREATE_TABLE);
+            database.execSQL((String) method.invoke(null));
+        } catch (RuntimeException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new UnableToCreateTableException("Unable to create table for " + tableObject.getName(), e);
+        }
+    }
+
+    private static Class<?> findShillelaghForClass(Class<?> clazz) throws ClassNotFoundException {
+        Class<?> shillelagh = CACHED_CLASSES.get(clazz);
+        if (shillelagh != null) {
+            return shillelagh;
+        }
+        final String className = clazz.getName();
+        shillelagh = Class.forName(className + ShillelaghProcessor.SUFFIX);
+        CACHED_CLASSES.put(clazz, shillelagh);
+        return shillelagh;
+    }
+
+    private static void log(String message) {
+        if (debug) Log.d(TAG, message);
+    }
+
+    public static final class UnableToCreateTableException extends RuntimeException {
+        UnableToCreateTableException(String message, Throwable cause) {
+            super(message, cause);
+        }
     }
 }
--- shillelagh/src/main/java/shillelagh/internal/ShillelaghInjector.java
+++ shillelagh/src/main/java/shillelagh/internal/ShillelaghInjector.java
@@ -1,4 +1,5 @@
 package shillelagh.internal;
 
 public class ShillelaghInjector {
+    public static final String CREATE_TABLE = "getCreateTableSql";
 }
