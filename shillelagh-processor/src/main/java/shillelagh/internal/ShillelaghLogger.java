bd8b5ed75578a6206134babf6ace8901a8d07cf8
Andrew Reitz <aj.reitz@gmail.com>
2014-09-04 07:49:30 -0500

    Made Logger Better.

--- shillelagh-processor/src/main/java/shillelagh/internal/ShillelaghLogger.java
+++ shillelagh-processor/src/main/java/shillelagh/internal/ShillelaghLogger.java
@@ -13,13 +13,27 @@ final class ShillelaghLogger {
     this.messenger = messenger;
   }
 
-  void d(String message) {
+  /**
+   * Print out debug logs, will only print if {@link shillelagh.internal.ShillelaghProcessor#DEBUG}
+   * is true.
+   */
+  void d(String message, Object... args) {
     if (DEBUG) {
-      messenger.printMessage(NOTE, message);
+      messenger.printMessage(NOTE, formatString(message, args));
     }
   }
 
-  void e(String message) {
-    messenger.printMessage(ERROR, message);
+  /** Print out notes */
+  void n(String message, Object... args) {
+    messenger.printMessage(NOTE, formatString(message, args));
+  }
+
+  /** Print out errors, this will stop the build from succeeding */
+  void e(String message, Object... args) {
+    messenger.printMessage(ERROR, formatString(message, args));
+  }
+
+  private String formatString(String message, Object... args) {
+    return args.length == 0 ? message : String.format(message, args);
   }
 }
