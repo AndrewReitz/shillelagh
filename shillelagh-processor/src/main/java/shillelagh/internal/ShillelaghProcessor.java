66ff1a4d78e8129e6865a7a7f49596839895755f
Andrew Reitz <aj.reitz@gmail.com>
2014-09-07 17:41:40 -0500

    Turned DEBUG flag off

--- shillelagh-processor/src/main/java/shillelagh/internal/ShillelaghProcessor.java
+++ shillelagh-processor/src/main/java/shillelagh/internal/ShillelaghProcessor.java
@@ -30,7 +30,7 @@ import shillelagh.Shillelagh;
 import shillelagh.Table;
 
 public final class ShillelaghProcessor extends AbstractProcessor {
-  static final boolean DEBUG = true;
+  static final boolean DEBUG = false;
 
   private Map<String, TableObject> oneToManyCache;
 
